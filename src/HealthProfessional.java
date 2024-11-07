9ef7e19222d3a411b0fd4dedf2af9c8ca3b90377
pattriciaruth <p.das.11@student.scu.edu.au>
2024-11-07 12:15:11 +0800

    start

--- src/HealthProfessional.java
+++ src/HealthProfessional.java
@@ -1,3 +1,30 @@
 public class HealthProfessional {
+    // Instance variables
+    private int ID;
+    private String name;
+    private String department; // Additional variable to describe the health professional
 
+    // Default constructor
+    public HealthProfessional() {
+        
+    }
+
+    // Parameterized constructor
+    public HealthProfessional(int ID, String name, String department) {
+        this.ID = ID;
+        this.name = name;
+        this.department = department;
+    }
+
+    public HealthProfessional(int ID) {
+        this.ID = ID;
+    }
+
+    // Method to print all instance variables
+    public void printInfo() {
+        System.out.println("ID: " + ID);
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+    }
 }
+
